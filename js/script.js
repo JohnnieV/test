95195a6f472b443606212054482c8436e7b05fc0
JohnnieV <johnnie.v1986@gmail.com>
2015-12-14 01:17:53 +0400

    1 push
    1 push

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.slick').slick({
+	arrows: false,	
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  autoplay: false,
+  autoplaySpeed: 3000,
+  adaptiveHeight: true
+});
---
+++ js/slick/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+		$('.slider').slick({				
+			variableWidth: true,	
+			arrows: true,
+			centerMode: true,
+	        centerPadding: '60px',
+	        slidesToShow: 3	        
+	});
+});
